9bcae11ce0eabd082ca22ac01e5479d001f5c0be
bigcanbigbig <jupiter2432321@gmail.com>
2016-06-16 12:16:08 +0800

    Add files via upload

--- index1.php
+++ index1.php
@@ -6,9 +6,18 @@
 		}
 		function mailRegex(){
 			$myfile = fopen("string.txt", "r");
+			if($myfile){
+			    while(!feof($myfile)) {
+			        $fc=fgetc($myfile);
+			        if(preg_match($fc,"[a-zA-Z0-9]")){
+			        	echo $fc;
+			        }
+			    }
+			}
 		}
 		function spiltroad(){
 			$myfile = fopen("road.txt", "r");
+
 		}
 	}
 ?>
